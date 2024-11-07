b90268b4d31d866c53d6ae85df2d5ccd31d71669
YASH2204V <yash.v220104@gmail.com>
2024-11-07 22:56:12 +0530

    embedded tailwindcss env.

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const flash = require("connect-flash");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render("index");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+})
